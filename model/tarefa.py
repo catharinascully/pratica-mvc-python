01ebf45d01f90c0a2899992f9d9c1ae9228ceaf8
catharinascully <catharina.scully@gmail.com>
2025-02-11 16:29:30 -0300

    update

---
+++ model/tarefa.py
@@ -0,0 +1,37 @@
+from database import Database
+
+class Tarefa:
+    def __init__(self, id, titulo, data_conclusao):
+        self.id = id
+        self.titulo = titulo
+        self.data_conclusao = data_conclusao
+
+    def salvarTarefa(self):
+        """Salva uma nova tarefa no banco de dados."""
+        db = Database()
+        db.conectar()
+
+        sql = 'INSERT INTO tarefa (titulo, data_conclusao) VALUES (%s, %s)' 
+        params = (self.titulo, self.data_conclusao)
+        db.executar(sql, params)
+        db.desconectar()
+    
+    def listarTarefas(self):
+        """Retorna uma lista com todas as tarefas cadastradas."""
+        db = Database()
+        db.conectar()
+
+        sql = 'SELECT id, titulo, data_conclusao FROM tarefa'
+        tarefas = db.consultar(sql)
+        db.desconectar()
+        return tarefas if tarefas else []
+    
+    def apagarTarefa(self):
+        """Apaga uma tarefa cadastrada no banco de dados."""
+        db = Database()
+        db.conectar()
+
+        sql = 'DELETE FROM tarefa WHERE id = %s'
+        params = (self.id,) # Precisa passar como tupla?
+        db.executar(sql, params)
+        db.desconectar()

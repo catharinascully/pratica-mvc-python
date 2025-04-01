5521b4c53c68eef2f96a5db67b76c47804b864b9
catharinascully <catharina.scully@gmail.com>
2025-04-01 16:32:32 -0300

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from turtle import title
 from flask import Flask, render_template, request, redirect, url_for
 from model.tarefa import Tarefa
 
@@ -20,7 +21,18 @@ def delete(idTarefa):
     Tarefa.apagarTarefa(idTarefa)
     return redirect(url_for('index'))
 
-@app.route('/update/<int:idTarefa>')
-def update(idTarefa):
-    Tarefa.atualizarTarefa(idTarefa)
-    return redirect(url_for('index'))
+@app.route('/update/<int:idTarefa>', methods = ['GET', 'POST'])
+def editar(idTarefa):
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        data_conclusao = request.form['data_conclusao']
+        tarefa = Tarefa(id = idTarefa, titulo = titulo, data_conclusao = data_conclusao)
+        tarefa.atualizar_tarefa()
+        return redirect(url_for('index'))
+
+    tarefa_selecionada = Tarefa.buscar_tarefa(idTarefa)
+    return render_template(
+        'index.html',
+        tarefa_selecionada = tarefa_selecionada,
+        title = 'Editando tarefa | Minhas tarefas'
+    )
--- model/tarefa.py
+++ model/tarefa.py
@@ -1,4 +1,5 @@
 from model.database import Database
+from datetime import datetime
 
 class Tarefa:
     def __init__(self, titulo, id=None, data_conclusao=None):
@@ -38,10 +39,38 @@ class Tarefa:
         db.executar(sql, params)
         db.desconectar()
     
-    def atualizarTarefa(self):
+    def buscar_tarefa(idTarefa):
+        """Busca uma tarefa específica do banco de dados e carrega na memória"""
         db = Database()
         db.conectar()
-        sql = 'UPDATE tarefa SET titulo = %s, data_conclusao = %s where id = %s'
-        params = (self.id, self.titulo, self.data_conclusao)
+
+        sql = 'SELECT id, titulo, data_conclusao FROM tarefa WHERE id = %s LIMIT 1'
+        params = (idTarefa,)
+        resultado = db.consultar(sql, params)
+        db.desconectar()
+
+        if resultado:
+            tarefa_selecionada = resultado[0]
+
+            data_formatada = (
+                tarefa_selecionada['data_conclusao'].strftime('%Y-%m-%d')
+                if isinstance(tarefa_selecionada['data_conclusao'], datetime)
+                else tarefa_selecionada['data_conclusao']
+            )
+
+            return Tarefa(
+                id = tarefa_selecionada['id'],
+                titulo = tarefa_selecionada['titulo'],
+                data_conclusao = data_formatada
+            )
+        return None
+    
+    def atualizar_tarefa(self):
+        """Altera as informações de uma tarefa"""
+        db = Database()
+        db.conectar()
+
+        sql = 'UPDATE tarefa SET titulo = %s, data_conclusao = %s WHERE id = %s'
+        params = (self.titulo, self.data_conclusao, self.id)
         db.executar(sql, params)
         db.desconectar()
